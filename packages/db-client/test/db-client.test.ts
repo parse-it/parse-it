5f02bedc7ea63da2fc4573a165d70bae689d85ce
Piers Concord <piers109uk@gmail.com>
2025-01-06 10:36:56 -0600

    fix: comment out test requiring DB

--- packages/db-client/test/db-client.test.ts
+++ packages/db-client/test/db-client.test.ts
@@ -3,19 +3,17 @@ import { describe, expect, it } from "vitest"
 
 describe("DBClient", () => {
   it("should run a select query", async () => {
-    const client = new Client({
-      user: "postgres",
-      database: "postgres",
-      password: "example",
-      host: "localhost",
-      port: 5432,
-    })
-
-    await client.connect()
-
-    const res = await client.query(
-      "SELECT name, email FROM users WHERE age > 18",
-    )
-    console.log(res.rows)
+    // const client = new Client({
+    //   user: "postgres",
+    //   database: "postgres",
+    //   password: "example",
+    //   host: "localhost",
+    //   port: 5432,
+    // })
+    // await client.connect()
+    // const res = await client.query(
+    //   "SELECT name, email FROM users WHERE age > 18",
+    // )
+    // console.log(res.rows)
   })
 })
